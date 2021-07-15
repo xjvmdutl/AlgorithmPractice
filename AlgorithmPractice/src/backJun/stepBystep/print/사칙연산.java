4d4d87d326ac2ed844b949126da4272598c53930
KimJunHo <widn45@naver.com>
2021-07-15 11:38:46 +0900

    day7

---
+++ AlgorithmPractice/src/backJun/stepBystep/print/사칙연산.java
@@ -0,0 +1,27 @@
+package backJun.stepBystep.print;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 사칙연산 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String str = reader.readLine();
+		int a = Integer.parseInt(str.split(" ")[0]);
+		int b = Integer.parseInt(str.split(" ")[1]);
+		System.out.println(solutiom(a,b));
+	}
+
+	private static String solutiom(int a, int b) {
+		StringBuilder sb = new StringBuilder();
+		sb.append((a+b) + "\n");
+		sb.append((a-b) + "\n");
+		sb.append((a*b) + "\n");
+		sb.append((a/b) + "\n");
+		sb.append(a%b);
+		return sb.toString();
+	}
+
+}
