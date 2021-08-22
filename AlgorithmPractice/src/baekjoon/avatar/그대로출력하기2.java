802c80b86aa0c4dc276a23b6f44b73d276adaf08
Kimjunho <widn45@naver.com>
2021-08-22 13:12:32 +0900

    d

--- AlgorithmPractice/src/baekjoon/avatar/그대로출력하기2.java
+++ AlgorithmPractice/src/baekjoon/avatar/그대로출력하기2.java
@@ -8,7 +8,9 @@ public class 그대로출력하기2 {
 	public static void main(String[] args) {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String str = null;
-	
+		while((str = reader.readLine()) != null) {
+			
+		}
 	
 	}
 
