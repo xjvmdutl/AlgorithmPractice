a1b89c4a6e2eb66615c9690d1fc756998cd00bbb
Kimjunho <widn45@naver.com>
2021-12-04 16:14:27 +0900

    commits

---
+++ AlgorithmPractice/src/progammers/level3/셔틀버스.java
@@ -0,0 +1,13 @@
+package progammers.level3;
+
+public class ¼ÅÆ²¹ö½º {
+	public String solution(int n, int t, int m, String[] timetable) {
+        String answer = "";
+        return answer;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
