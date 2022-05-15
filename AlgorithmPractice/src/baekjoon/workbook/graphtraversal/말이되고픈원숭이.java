a9521639c76e015ac18e36778e3ce59d8ac566ad
Kimjunho <widn45@naver.com>
2022-05-15 19:55:28 +0900

    Commit Test(Token 재발행)

---
+++ AlgorithmPractice/src/baekjoon/workbook/graphtraversal/말이되고픈원숭이.java
@@ -0,0 +1,13 @@
+package baekjoon.workbook.graphtraversal;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ¸»ÀÌµÇ°íÇÂ¿ø¼şÀÌ {
+
+	public static void main(String[] args) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+}
